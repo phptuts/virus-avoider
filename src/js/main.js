f8fed66bb67e6c451f3b2d3943ecea19ab81f1a9
Noah Glaser <glaserpower@gmail.com>
2020-04-09 22:03:03 -0700

    getting virus and gravity to work

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,19 @@ console.log('hello world');
 import Phaser from 'phaser';
 import sceneMain from './sceneMain';
 window.game;
-window.onload = function() {
+window.onload = function () {
   var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     parent: 'phaser-game',
-    scene: [sceneMain]
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 300 },
+      },
+    },
+    scene: [sceneMain],
   };
 
   window.game = new Phaser.Game(config);
--- src/js/sceneMain.js
+++ src/js/sceneMain.js
@@ -5,13 +5,48 @@ export default class SceneMain extends Phaser.Scene {
 
   preload() {
     this.load.image('background', 'images/background.jpg');
+    this.load.image('player', 'images/penguin.png');
+    this.load.image('virus', 'images/virus.png');
   }
 
   create() {
     let bg = this.add.sprite(0, 0, 'background');
     bg.setOrigin(0, 0);
     bg.setScale(0.8);
+    this.player = this.physics.add.sprite(690, 550, 'player');
+    this.player.setScale(0.2);
+    this.player.setCollideWorldBounds(true);
+    this.virus = this.physics.add.group({
+      key: 'virus',
+      setScale: { x: 0.5, y: 0.5 },
+      gravityY: 0,
+      collideWorldBounds: true,
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
-  update() {}
+  update() {
+    this.controlPlayer();
+  }
+
+  controlPlayer() {
+    console.log(this.player.y);
+    if (this.cursors.up.isDown && this.player.y > 570) {
+      this.player.setVelocityY(-500);
+      return;
+    }
+
+    if (this.cursors.right.isDown) {
+      this.player.setVelocityX(100);
+      return;
+    }
+
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-100);
+      return;
+    }
+
+    this.player.setVelocityX(0);
+  }
 }
